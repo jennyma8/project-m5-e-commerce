da1bf0afcd3fc22af40a85b7fc2c447b46dd8e15
Truong Dao <truong.dao.do@gmail.com>
2020-09-14 22:17:29 -0400

    ProductItemPage

--- client/src/components/Layout/Layout.js
+++ client/src/components/Layout/Layout.js
@@ -4,6 +4,7 @@ import styled from "styled-components";
 import HomePage from "../../containers/HomePage";
 import ProductsPage from "../../containers/ProductsPage";
 import Nav from "../Nav";
+import ProductItemPage from "../../containers/ProductItemPage";
 
 const Layout = () => {
   return (
@@ -20,8 +21,8 @@ const Layout = () => {
           <Route path="/about">
             <h1>this is the careers page</h1>
           </Route>
-          <Route path="/products/:id">
-            <h1>This is the detailed product page</h1>
+          <Route path="/items/:id">
+            <ProductItemPage />
           </Route>
           <Route path="/products">
             <ProductsPage />
