4e3cfe5b50af8e20341d70138e4aea40a024ee19
iescalante <ivanenriqueescalantecornejo@gmail.com>
2020-09-16 19:32:33 -0400

    cleanup price into Number for categories

--- server/routes/items/categories.js
+++ server/routes/items/categories.js
@@ -5,6 +5,13 @@ module.exports = (req, res) => {
   const itemCategoryParam = req.params.category;
   const items = DATA.filter((item) => {
     return item.category.toLowerCase() === itemCategoryParam.toLowerCase();
+  }).map((item) => {
+    const price = parseFloat(item.price.substring(1));
+    return {
+      ...item,
+      price: price,
+    };
   });
+  console.log(items);
   res.status(200).json({ items });
 };
