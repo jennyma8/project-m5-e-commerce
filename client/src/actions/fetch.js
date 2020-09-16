1c7510118f2096fafea891590d55abbd0ca60eb8
Thinh Nguyen <thinh-nguyen@hotmail.com>
2020-09-15 22:40:48 -0400

    added actions/reducers to redux for categories

--- client/src/actions/fetch.js
+++ client/src/actions/fetch.js
@@ -1,3 +1,5 @@
+// ###################### FETCH COMPANIES ################################
+
 export const requestCompanies = () => ({
   type: "REQUEST_COMPANIES_DATA",
 });
@@ -11,6 +13,23 @@ export const receiveCompaniesError = () => ({
   type: "REQUEST_COMPANIES_DATA_ERROR",
 });
 
+// ###################### FETCH CATEGORIES ################################
+
+export const requestCategories = () => ({
+  type: "REQUEST_CATEGORIES_DATA",
+});
+
+export const receiveCategories = (categories) => ({
+  type: "RECEIVE_CATEGORIES_DATA",
+  categories,
+});
+
+export const receiveCategoriesError = () => ({
+  type: "REQUEST_CATEGORIES_DATA_ERROR",
+});
+
+// ###################### FETCH ALL ITEMS ################################
+
 export const requestItems = () => ({
   type: "REQUEST_ITEMS_DATA",
 });
@@ -23,6 +42,8 @@ export const receiveItems = (items) => ({
 export const receiveItemsError = () => ({
   type: "REQUEST_ITEMS_DATA_ERROR",
 });
+
+// ###################### FETCH SINGLE ITEM ################################
 
 export const requestProductItem = () => ({
   type: "REQUEST_PRODUCT_ITEM_DATA",
