2d48b6960b6d2e080f4b1eddefb3fbb72b45a0d2
jenny ma <jenny_lau_8@hotmail.com>
2020-09-16 23:21:07 -0400

    categories

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -1,2 +1,3 @@
 export { default as Logo } from "./logo1.png";
 export { default as Video } from "./Landscape.mp4";
+//https://www.pexels.com/video/beautiful-timelapse-of-the-night-sky-with-reflections-in-a-lake-857251/
