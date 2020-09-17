c13c145c3fcb6a18b795bfe231cbb0b3fef38c37
Thinh Nguyen <thinh-nguyen@hotmail.com>
2020-09-16 23:43:27 -0400

    updated themes

--- client/src/components/THEMES.js
+++ client/src/components/THEMES.js
@@ -1,5 +1,7 @@
 export const THEMES = {
-  Primary: "#4fc4cf",
-  Secondary: "#994ff3",
+  // Primary: "#4fc4cf",
+  Primary: "#3C3744",
+  // Secondary: "#994ff3",
+  Secondary: "goldenrod",
   Tertiary: "#fbdd74",
 };
