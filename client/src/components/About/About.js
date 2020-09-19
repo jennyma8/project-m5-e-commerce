343b084fe47427bbba5a71865601bec55522dec4
jenny ma <jenny_lau_8@hotmail.com>
2020-09-19 19:04:20 -0400

    about us complete

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -1,6 +1,5 @@
 import React from "react";
 import styled from "styled-components";
-import { NavLink } from "react-router-dom";
 import { Ivan, Thinh, Truong, Jenny } from "../../assets";
 import { FiGithub, FiTwitter } from "react-icons/fi";
 import { AiFillLinkedin } from "react-icons/ai";
