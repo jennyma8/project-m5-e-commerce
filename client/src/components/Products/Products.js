fce13dc976005cfc9f693013f2c9863c08999dbd
Thinh Nguyen <thinh-nguyen@hotmail.com>
2020-09-20 01:54:50 -0400

    more cleanup

--- client/src/components/Products/Products.js
+++ client/src/components/Products/Products.js
@@ -1,36 +1,8 @@
 import React from "react";
 import styled from "styled-components";
 import Product from "./Product";
-import Spinner from "../UI/Spinner";
-
-import { useSelector, useDispatch } from "react-redux";
-import { requestItems, receiveItems, receiveItemsError } from "../../actions";
 
 const Products = (props) => {
-  // const dispatch = useDispatch();
-
-  // const ITEMS = useSelector((state) => state.DATA.allProducts);
-  // const STATUS = useSelector((state) => state.DATA.status);
-
-  // React.useEffect(() => {
-  //   // This should be fetching "/items"
-  //   const URL = window.location.pathname;
-  //   try {
-  //     dispatch(requestItems());
-  //     fetch(URL)
-  //       .then((res) => res.json())
-  //       // .then((json) => console.log(json));
-  //       .then((json) => dispatch(receiveItems(json)));
-  //   } catch (error) {
-  //     console.log("error");
-  //     dispatch(receiveItemsError());
-  //   }
-  // }, []);
-
-  // if (STATUS === "loading" || !ITEMS) {
-  //   return <Spinner />;
-  // }
-
   return (
     <ProductsGrid>
       {props.data.items.map((item) => {
@@ -43,9 +15,6 @@ const Products = (props) => {
           />
         );
       })}
-      {/* {ITEMS.items.map((item) => {
-        return <Product key={item._id} data={item} stock={item.numInStock} />;
-      })} */}
     </ProductsGrid>
   );
 };
