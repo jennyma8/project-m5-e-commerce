7c09e3943d44c99d35dcd1547ea12c13b10b8a74
jenny ma <jenny_lau_8@hotmail.com>
2020-09-19 21:04:58 -0400

    careers

---
+++ client/src/components/Careers/Careers.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+
+const Careers = () => {
+  return (
+    <>
+      <Wrapper>
+        <div>hello</div>
+      </Wrapper>
+    </>
+  );
+};
+
+const Wrapper = styled.div``;
+export default Careers;
