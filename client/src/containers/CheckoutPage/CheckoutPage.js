b969e850c3466ba04c7150ce3158dd842331fcc2
Thinh Nguyen <thinh-nguyen@hotmail.com>
2020-09-21 19:06:23 -0400

    created CheckoutPage container component

---
+++ client/src/containers/CheckoutPage/CheckoutPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+import PageContainer from "../../components/UI/PageContainer";
+
+const CheckoutPage = () => {
+  return (
+    <PageContainer>
+      <h1>Hello World</h1>
+    </PageContainer>
+  );
+};
+
+export default CheckoutPage;
