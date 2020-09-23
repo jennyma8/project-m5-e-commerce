14a28e14d5239e38fa0181e23198fdefda474548
jenny ma <jenny_lau_8@hotmail.com>
2020-09-23 18:00:59 -0400

    homepage text

--- client/src/containers/HomePage/HomePage.js
+++ client/src/containers/HomePage/HomePage.js
@@ -70,6 +70,9 @@ const HomePage = () => {
             <source src={Video} type="video/mp4" />
           </VideoSrc>
           <Overlay>Best Selection of Watches</Overlay>
+          <Overlay2>Large Variety of Fitness Wearables</Overlay2>
+          <Overlay3>Products for Pet and Animals</Overlay3>
+          <Overlay4>Heart Rate Monitor</Overlay4>
         </VideoContainer>
         <Categories>Find What You Need</Categories>
         <WrapperCategories>
@@ -128,6 +131,36 @@ const Slide = keyframes`
 const Overlay = styled.div`
   position: absolute;
   top: 30%;
+  right: 0%;
+  z-index: 1;
+  color: white;
+  font-size: 30px;
+  animation: ${Slide} 12s linear infinite;
+`;
+
+const Overlay2 = styled.div`
+  position: absolute;
+  top: 50%;
+  right: 0%;
+  z-index: 1;
+  color: white;
+  font-size: 30px;
+  animation: ${Slide} 12s linear infinite;
+`;
+
+const Overlay3 = styled.div`
+  position: absolute;
+  top: 20%;
+  right: 0%;
+  z-index: 1;
+  color: white;
+  font-size: 30px;
+  animation: ${Slide} 12s linear infinite;
+`;
+
+const Overlay4 = styled.div`
+  position: absolute;
+  top: 40%;
   right: 0%;
   z-index: 1;
   color: white;
